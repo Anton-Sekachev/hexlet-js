d6ac383156623c589cee981342d2bfbfa39b762e
Anton Sekachev <a9185080045@gmail.com>
2023-09-28 14:40:05 +0300

    modified capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,21 @@
+import { strict as assert } from 'node:assert';
 // eslint-disable-next-line import/extensions
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+// assert(capitalize('') === '');
+// assert(capitalize('hello') === 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// eslint-disable-next-line import/extensions
+// import capitalize from '../src/capitalize.js';
+
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
+
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
 
 console.log('Все тесты пройдены!');
